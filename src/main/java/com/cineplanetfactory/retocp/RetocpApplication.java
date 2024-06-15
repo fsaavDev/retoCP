e0befcf1cf9bf4d9113e5e233bb79e873d86f5b0
Flavio Saavedra <f.saavedra1810@gmail.com>
2024-06-14 20:09:02 -0500

    adding: initial configuration and files

---
+++ src/main/java/com/cineplanetfactory/retocp/RetocpApplication.java
@@ -0,0 +1,13 @@
+package com.cineplanetfactory.retocp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetocpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetocpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cineplanetfactory/retocp/RetocpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineplanetfactory.retocp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetocpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
